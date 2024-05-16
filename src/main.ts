2068aad8128f85f231b284d1d4339e9c4f364377
Jorge Martín Capcha <jmcapcha@gmail.com>
2024-05-16 16:29:15 -0500

    ADD: pruebas to main ts

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,13 @@
 // Iniciar un contador de 10 segundos antes de empezar el juego
 // Inciar un intervalo para mostras los segundos restantes en la consola
 // Recuerda 'cancelar' el intervalo cuando llegue a 0 segundos
+
+import { Pokemon } from "./pokemon";
+
+const poke = new Pokemon("Spearow");
+console.log(poke.type);
+console.log(poke.growthRate);
+console.log(poke.baseExp);
+console.log(poke.experiencePoints);
+console.log(poke.setCurrentMove("peck"));
+console.log(poke.isFainted);
