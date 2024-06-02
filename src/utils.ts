cb6e624536ae0417e8c75e4cb9a842d6e95ae831
Jorge Martín Capcha <jmcapcha@gmail.com>
2024-06-01 19:29:04 -0500

    ADD: capitalize function to utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,8 @@
-export default function randomBetween(min: number, max: number): number {
+export function randomBetween(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export function capitalize(str: string): string {
+  str = str.toLowerCase();
+  return str.replace(/\b\w/g, (char) => char.toUpperCase());
 }
